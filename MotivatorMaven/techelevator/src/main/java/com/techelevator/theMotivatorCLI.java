1b1adef522df503f773d506bc12422043f2a94f0
Jonathon McCollough <jonathonmccollough@gmail.com>
2020-03-10 16:33:40 -0400

    saving CLI under the Spring capable file

--- MotivatorMaven/techelevator/src/main/java/com/techelevator/theMotivatorCLI.java
+++ MotivatorMaven/techelevator/src/main/java/com/techelevator/theMotivatorCLI.java
@@ -16,10 +16,17 @@ public class theMotivatorCLI {
 		complimentsList.add("looking real good!");
 		complimentsList.add("you are so brilliant!");
 		complimentsList.add("never doubt yourself!");
+		complimentsList.add("your limitation—it's only your imagination!");
+		complimentsList.add("push yourself, because no one else is going to do it for you!");
+		complimentsList.add("the harder you work for something, the greater you'll feel when you achieve it!");
 
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("Please, enter your name: ");
 		String userName = userInput.nextLine();
+		if(userName.contentEquals("")) {
+			userName = "Mysterious person";
+		}
+		userName = userName.substring(0, 1).toUpperCase() + userName.substring(1);
 		System.out.println(userName + ", " + complimentsList.get((new Random().nextInt(complimentsList.size()))));
 		System.out.println("Press 1 for more motivation or Press 2 to Exit");
 		String userInput2 = userInput.nextLine();
@@ -27,6 +34,8 @@ public class theMotivatorCLI {
 		while (!exit) {
 			if (userInput2.contentEquals("2")) {
 				exit = true;
+				System.out.println("Have a great day " + userName + "!");
+				System.exit(0);
 			}
 			System.out.println(userName + ", " + complimentsList.get((new Random().nextInt(complimentsList.size()))));
 			System.out.println("Press 1 for more motivation or 2 to Exit.");
