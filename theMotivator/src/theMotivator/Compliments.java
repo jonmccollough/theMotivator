559b2569a9e6fcc3d553a325632c377d50ce1b28
Jonathon McCollough <jonathonmccollough@gmail.com>
2020-03-10 15:45:45 -0400

    committing first working save to gitHub

---
+++ theMotivator/src/theMotivator/Compliments.java
@@ -0,0 +1,11 @@
+package theMotivator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Compliments {
+	
+	List<String> cList = new ArrayList<String>();
+		//complimentsList.add("text");
+
+}
